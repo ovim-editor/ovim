5e0362a3a1d3d69412cdd37fe2f8e5ec6fe3feef
Adrian Helvik <not-disclosed>
2026-01-31 11:26:52 +0100

    refactor: extract ThemeState sub-struct from Editor
    Move color_scheme_registry and current_color_scheme into
    a dedicated ThemeState struct.

--- src/editor/theme.rs
+++ src/editor/theme.rs
@@ -5,13 +5,13 @@ use anyhow::Result;
 impl Editor {
     /// Gets the current color scheme
     pub fn get_color_scheme(&self) -> Option<&ColorScheme> {
-        self.color_scheme_registry.get(&self.current_color_scheme)
+        self.theme.color_scheme_registry.get(&self.theme.current_color_scheme)
     }
 
     /// Sets the color scheme by name
     pub fn set_color_scheme(&mut self, name: &str) -> Result<()> {
-        if self.color_scheme_registry.get(name).is_some() {
-            self.current_color_scheme = name.to_string();
+        if self.theme.color_scheme_registry.get(name).is_some() {
+            self.theme.current_color_scheme = name.to_string();
             Ok(())
         } else {
             Err(anyhow::anyhow!("Color scheme '{}' not found", name))
@@ -20,11 +20,11 @@ impl Editor {
 
     /// Lists all available color scheme names
     pub fn list_color_schemes(&self) -> Vec<&str> {
-        self.color_scheme_registry.list_names()
+        self.theme.color_scheme_registry.list_names()
     }
 
     /// Gets the current color scheme name
     pub fn current_color_scheme_name(&self) -> &str {
-        &self.current_color_scheme
+        &self.theme.current_color_scheme
     }
 }
