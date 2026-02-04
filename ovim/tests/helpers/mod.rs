83be5bac58be0597bdba8118ddead8dd88319b63
Adrian Helvik <not-disclosed>
2026-02-04 19:02:30 +0100

    test: allow editor_test fixtures without commas

--- ovim/tests/helpers/mod.rs
+++ ovim/tests/helpers/mod.rs
@@ -13,6 +13,62 @@ use ovim::mode::Mode;
 #[macro_export]
 macro_rules! editor_test {
     (
+        given { $( $given_line:literal )+ }
+        keys $keys:literal
+        expect $expect_mode:ident { $( $expect_line:literal )+ }
+        $(,)?
+    ) => {{
+        let given_pairs: &[&str] = &[ $( $given_line ),+ ];
+        let expect_pairs: &[&str] = &[ $( $expect_line ),+ ];
+        let given_fixture =
+            $crate::helpers::dsl::fixture_from_pairs(::ovim::mode::Mode::Normal, given_pairs);
+        let expect_fixture =
+            $crate::helpers::dsl::fixture_from_pairs(::ovim::mode::Mode::$expect_mode, expect_pairs);
+        $crate::helpers::dsl::run_editor_test_case(given_fixture, $keys, expect_fixture);
+    }};
+
+    (
+        given { $( $given_line:literal )+ }
+        when $keys:literal
+        expect $expect_mode:ident { $( $expect_line:literal )+ }
+        $(,)?
+    ) => {{
+        $crate::editor_test! {
+            given { $( $given_line )+ }
+            keys $keys
+            expect $expect_mode { $( $expect_line )+ }
+        }
+    }};
+
+    (
+        given { $( $given_line:literal )+ }
+        $(
+            keys $step_keys:literal
+            expect $step_mode:ident { $( $step_line:literal )+ }
+        )+
+        $(,)?
+    ) => {{
+        let given_pairs: &[&str] = &[ $( $given_line ),+ ];
+        let given_fixture =
+            $crate::helpers::dsl::fixture_from_pairs(::ovim::mode::Mode::Normal, given_pairs);
+
+        let steps: Vec<(&'static str, $crate::helpers::dsl::Fixture)> = vec![
+            $(
+                {
+                    let expect_pairs: &[&str] = &[ $( $step_line ),+ ];
+                    let expect_fixture = $crate::helpers::dsl::fixture_from_pairs(
+                        ::ovim::mode::Mode::$step_mode,
+                        expect_pairs,
+                    );
+                    ($step_keys, expect_fixture)
+                }
+            ),+
+        ];
+
+        $crate::helpers::dsl::run_editor_test_steps(given_fixture, steps);
+    }};
+
+    (
         given $given_mode:ident { $( $given_line:literal ),* $(,)? }
         keys $keys:literal
         expect $expect_mode:ident { $( $expect_line:literal ),* $(,)? }
